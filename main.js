34c412b6e656ec66550dca2cf55bfbcfa9dbc636
Julie-Charles16 <juliecharle2716@gmail.com>
2023-04-03 12:50:01 +0200

    bof

--- main.js
+++ main.js
@@ -1,16 +1,47 @@
-function verifyReply(){
-    var proposedValue = valueField.value;
-    var pattern = /^\d+$/;
+var randomNumber = Math.floor(Math.random() * 10) + 1;
+var guessCount = 0;
+var guessLimit = 3;
+var guesses = [];
 
-    if(pattern.test(proposedValue)){
-        numberTry++;
-    }
+document.getElementById("replay").style.display = "none";
 
+const button = document.getElementById('btnTest');
+button.onclick = function() {
+    tester();
 }
 
-var maxValue = 10;
-var valueToFind = ((Math.random() * maxValue) * 1);
-var numberTry = 0;
-var valueField = document.getElementById(elementid : valuefield);
+function tester() {
 
-if(valueField)
+    var uNum = parseInt(document.getElementById("userInput").value);
+    console.log(uNum)
+    guessCount++;
+    
+   
+    if (guessCount > 3) { 
+        rejouer();  
+    }
+
+    if (uNum = randomNumber) {
+        document.getElementById("childResult").innerHTML = "Bravo vous avez gagné en  " + guessCount + "Essai!";
+        rejouer();
+    }
+    else if (guessCount === guessLimit) {
+        document.getElementById("childResult").innerHTML = "Perdu! le numéro était " + randomNumber + ".";
+    } else if (uNum < randomNumber) {
+        document.getElementById("childResult").innerHTML = "Essai " + guessCount + ": " + uNum + " est trop petit. Essayez encore!";
+    } else if (uNum > randomNumber) {
+        document.getElementById("childResult").innerHTML = "Essai " + guessCount + ": " + uNum + " est trop grand. Essayez encore!";
+    }
+}
+
+function rejouer() {
+    var title = document.querySelector("h2");
+        title.parentNode.removeChild(title);
+        document.getElementById("replay").style.display = "block";
+    randomNumber = Math.floor(Math.random() * 10) + 1;
+    guessCount = 0;
+    guesses = [];
+    document.getElementById("userInput").value = "0"
+    document.getElementById("childResult").innerHTML = "";
+}
+
